c3c67c00211ec0efefd2494bf0276e8cb85c09b2
Cuti27 <48327467+Cuti27@users.noreply.github.com>
2022-09-17 15:43:08 +0200

    - Init

---
+++ app/components/Player.vue
@@ -0,0 +1,31 @@
+<template>
+    <FlexboxLayout flexDirection="column" justifyContent="center"
+        alignItems="center">
+        <Label textWrap="true" text="Añade nuevos jugadores"
+            class="h2 description-label"></Label>
+            
+        <TextField v-model="playerName" hint="Nombre del jugador"></TextField>
+        <Button @tap="AddPlayer">Agregar</Button>
+    </FlexboxLayout>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                playerName: ""
+            };
+        },
+        methods: {
+            AddPlayer() {
+                if (this.playerName != "") {
+                    this.$emit("add", this.playerName);
+                    this.playerName = "";
+                }
+            }
+        }
+    };
+</script>
+
+<style scoped>
+</style>
